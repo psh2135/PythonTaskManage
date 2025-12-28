5b7e039a721f5f692830fe9838bef5f7ed7cb336
329332571 <S026635836@gmail.com>
2025-12-28 14:49:51 +0200

    add models

--- EndProject/models.py
+++ EndProject/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
 # Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    describe = models.CharField(max_length=1000)
+    deadline = models.DateTimeField()
+    myTeam = models.ForeignKey('Team', on_delete=models.CASCADE)
+   # status = models.enums
+
+    def __str__(self):
+        return f'{self.title}\n {self.describe}'
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return f'{self.name}'
